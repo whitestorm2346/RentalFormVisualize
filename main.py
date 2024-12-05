0183d7477d822b1b1a8d0196ec3743be74121ffe
whitestorm2346 <andrew770426@gmail.com>
2024-12-05 10:52:30 +0800

    Modified the use of title, and added the process bar into the main loop

--- main.py
+++ main.py
@@ -9,29 +9,32 @@ import chromedriver_autoinstaller
 import folium
 import re
 from time import sleep
+from bs4 import BeautifulSoup
+from tqdm import tqdm # process bar
 
 # DATA_FILE = "113學年度學生居住調查暨校外賃居安全自評表_部份資料測試用.xlsx"
 DATA_FILE = "113學年度學生居住調查暨校外賃居安全自評表.xlsx"
 GOOGLE_MAP = "https://www.google.com.tw/maps"
 
 
-ID_COL_IDX = 64
-FINISH_DT_COL_IDX = 2
-CURRENT_RESIDENSY_COL_IDX = 7
-ADDRESS_COL_IDX = 9
-PROPERTY_LABEL_COL_IDX = 10
-PROPERTY_TYPE_COL_IDX = 14
+ID_COL = "學號 Student ID（共9碼不要少了）"
+FINISH_DT_COL = "完成時間"
+CURRENT_RESIDENSY_COL = "目前居住位置 Current Residency"
+ADDRESS_COL = "租屋地址（請詳填住址，所有數字用「半型阿拉伯數字」填入，例如：新北市淡水區水源街2段1號）address"
+PROPERTY_LABEL_COL = "社區大樓名稱（沒有則填「無」）community name (If not fill in none)"
+PROPERTY_TYPE_COL = "房屋類型（公寓無電梯；平房只有一樓）Property Type\n"
+SELF_SAFETY_CKECK_COL = "經過自我安全檢視後，我覺得... After a self-safety check, I think..."
 
 
 def remove_duplicate(title, data):
     # data[title[FINISH_DT_COL_IDX]] = pd.to_datetime(data[title[FINISH_DT_COL_IDX]], format='%m/%d/%y %H:%M')
-    data[title[FINISH_DT_COL_IDX]] = pd.to_datetime(data[title[FINISH_DT_COL_IDX]], format='%m/%d/%y %H:%M:%S')
-    latest_entries = data.loc[data.groupby(title[ID_COL_IDX])[title[FINISH_DT_COL_IDX]].idxmax()]
+    data[FINISH_DT_COL] = pd.to_datetime(data[FINISH_DT_COL], format='%m/%d/%y %H:%M:%S')
+    latest_entries = data.loc[data.groupby(ID_COL)[FINISH_DT_COL].idxmax()]
 
     return latest_entries
 
 def residency_filter(title, data):
-    filtered_data = data[data[title[CURRENT_RESIDENSY_COL_IDX]].str.contains('校外租屋', na=False)]
+    filtered_data = data[data[CURRENT_RESIDENSY_COL].str.contains('校外租屋', na=False)]
 
     return filtered_data
 
@@ -60,20 +63,44 @@ def get_lat_lng(driver, address):
         pass
 
     sleep(1)
+
+    # 處理沒有找到特定地點
+
     new_url = driver.current_url
 
     driver.refresh() # 確保大頭針在視窗中央 -> 確保經緯度正確
 
+    counts = 1
+
     while driver.current_url == new_url:
-        pass
+        if counts >= 30:
+            return False
+
+        sleep(0.1)
+        counts += 1
+        
 
     return extract_lat_lng(driver.current_url)
+
+def make_functional_map(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        soup = BeautifulSoup(file, 'html.parser')
     
+    button_html = '''
+        <button onclick="alert('Button clicked!')" style="position:absolute; top:10px; left:10px; z-index:9999;">
+            Click Me
+        </button>
+    '''
+    soup.body.insert(0, BeautifulSoup(button_html, 'html.parser'))
+
+    with open(f'new_{file_name}.html', 'w', encoding='utf-8') as file:
+        file.write(str(soup))
 
 if __name__ == "__main__":
     try:
         df = pd.read_excel(DATA_FILE, header=None)
     except Exception as e:
+        print(e)
         print('Excel file not found!')
         exit(1)
 
@@ -94,13 +121,14 @@ if __name__ == "__main__":
     map_for_students = folium.Map(location=tku_coord, zoom_start=16, tiles="OpenStreetMap")
     map_for_teachers = folium.Map(location=tku_coord, zoom_start=16, tiles="OpenStreetMap")
 
-    for index, row in filtered_data.iterrows():
-        address = row[title[4]]
-        property_label = row[title[5]]
-        property_type = row[title[6]]
-        self_safety_check = row[title[7]]
+    for index, row in tqdm(filtered_data.iterrows(), total=len(filtered_data), desc="Processing addresses"):
+        address = row[ADDRESS_COL]
+        property_label = row[PROPERTY_LABEL_COL]
+        property_type = row[PROPERTY_TYPE_COL]
+        self_safety_check = row[SELF_SAFETY_CKECK_COL]
 
         coord = get_lat_lng(driver, address)
+
         sleep(1)
 
         if coord:
